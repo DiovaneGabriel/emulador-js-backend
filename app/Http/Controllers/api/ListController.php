137dd9853eb69e56bfa7b0a5d0af268333d24bdf
DiovaneGabriel <diovane.gabriel@gmail.com>
2023-05-21 22:32:28 -0300

    mvp

--- app/Http/Controllers/api/ListController.php
+++ app/Http/Controllers/api/ListController.php
@@ -18,11 +18,25 @@ class ListController extends ApiController {
         return self::return_json(Game::whereConsoleId($consoleId)->get());
     }
     public function getCartridgeGames($cartridgeKey) {
+        $result = [];
         $cartridge = Cartridge::where('key', $cartridgeKey)->first();
         if ($cartridge) {
-            return self::return_json(CartridgeGame::whereCartridgeId($cartridge->id)->with(['game'])->get());
-        }
+            $games = CartridgeGame::whereCartridgeId($cartridge->id)->with(['game'])->get();
 
-        return false;
+            if (count($games)) {
+                $pages = ceil(count($games) / 10);
+
+                for ($i = 0; $i < $pages; $i++) {
+                    foreach ($games as $k => $game) {
+                        if ($k < ($i + 1) * 10 && $k >= $i * 10) {
+                            $result['pages'][$i]['id'] = ($i + 1);
+                            $result['pages'][$i]['games'][] = $game;
+                            $result['count_games'] = count($games);
+                        }
+                    }
+                }
+            }
+        }
+        return self::return_json($result);
     }
 }
